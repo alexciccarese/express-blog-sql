4f5e3e3be2414474d8bcc0ec55f99ef780ae52ee
alexciccarese <alex.ciccarese@gmail.com>
2025-04-14 17:28:55 +0200

    Refactor postsController and db connection setup for improved readability and error handling

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,88 +1,97 @@
+const connection = require('../data/db')
 const menu = require('../data/menu')
 
 function index(req, res) {
-  res.json(menu)
-}
+
+  const sql = 'SELECT * FROM posts'
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: 'Query Failed' })
+
+    console.log(results);
+    res.json(results)
+
+  });
+  }
 
 function show(req, res) {
-  const postId = Number(req.params.id)
+      const postId = Number(req.params.id)
 
-  //troviamo il post tramite slug
-  const post = menu.find(post => post.id === postId)
-  console.log(post);
+      //troviamo il post tramite slug
+      const post = menu.find(post => post.id === postId)
+      console.log(post);
 
 
-  res.json(post)
-}
+      res.json(post)
+    }
 
 function store(req, res) {
-  const newId = menu.length ? menu[menu.length - 1].id + 1 : 1;
-  const newSlug = req.body.title.toLowerCase()
+      const newId = menu.length ? menu[menu.length - 1].id + 1 : 1;
+      const newSlug = req.body.title.toLowerCase()
 
-  const newPost = {
-    id: newId,
-    title: req.body.title,
-    slug: newSlug,
-    content: req.body.content,
-    image: req.body.image,
-    tags: req.body.tags
-  }
+      const newPost = {
+        id: newId,
+        title: req.body.title,
+        slug: newSlug,
+        content: req.body.content,
+        image: req.body.image,
+        tags: req.body.tags
+      }
 
-  //aggiungiamo il nuovo post al menu
-  menu.push(newPost)
+      //aggiungiamo il nuovo post al menu
+      menu.push(newPost)
 
-  console.log(menu);
+      console.log(menu);
 
-  res.status(201)
-  res.json(newPost)
-  
+      res.status(201)
+      res.json(newPost)
 
-}
+
+    }
 
 function update(req, res) {
-  const postId = Number(req.params.id)
-  const post = menu.find(post => post.id === postId)
+      const postId = Number(req.params.id)
+      const post = menu.find(post => post.id === postId)
 
-  //404 error
-  if(!post) {
-    return res.status(404).json({
-      error: '404 not found',
-      message: 'Post not found'
-    })
-  }
+      //404 error
+      if (!post) {
+        return res.status(404).json({
+          error: '404 not found',
+          message: 'Post not found'
+        })
+      }
 
-  //aggiorniamo il post
-  post.title = req.body.title
-  post.content = req.body.content
-  post.image = req.body.image
-  post.tags = req.body.tags
+      //aggiorniamo il post
+      post.title = req.body.title
+      post.content = req.body.content
+      post.image = req.body.image
+      post.tags = req.body.tags
 
-  //check the menu
-  res.json(post)
-}
+      //check the menu
+      res.json(post)
+    }
 
 function modify(req, res) {
-  res.send(`Modify the post with an id: ${req.params.id}`)
-}
+      res.send(`Modify the post with an id: ${req.params.id}`)
+    }
 
 function destroy(req, res) {
-  const postId = Number(req.params.id)
+      const postId = Number(req.params.id)
 
-  const post = menu.find(post => post.id === postId)
-  console.log(post);
+      const post = menu.find(post => post.id === postId)
+      console.log(post);
 
-  //rimuoviamo il singolo id dall'arrey
-  menu.splice(menu.indexOf(post), 1)
-  console.log(menu);
+      //rimuoviamo il singolo id dall'arrey
+      menu.splice(menu.indexOf(post), 1)
+      console.log(menu);
 
-  res.sendStatus(204)
-}
+      res.sendStatus(204)
+    }
 
 module.exports = {
-  index,
-  show,
-  store,
-  update,
-  modify,
-  destroy
-}
+      index,
+      show,
+      store,
+      update,
+      modify,
+      destroy
+    }
